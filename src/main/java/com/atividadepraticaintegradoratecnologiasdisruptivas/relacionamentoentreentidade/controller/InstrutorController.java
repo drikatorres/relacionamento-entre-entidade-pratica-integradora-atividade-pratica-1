6b801e5d3ce2497066fc907503bbb1ab7a5abd97
drikatorres <torrescruz.drika@gmail.com>
2023-05-05 22:39:38 -0300

    adicionando a camada controller

--- src/main/java/com/atividadepraticaintegradoratecnologiasdisruptivas/relacionamentoentreentidade/controller/InstrutorController.java
+++ src/main/java/com/atividadepraticaintegradoratecnologiasdisruptivas/relacionamentoentreentidade/controller/InstrutorController.java
@@ -1,7 +1,63 @@
 package com.atividadepraticaintegradoratecnologiasdisruptivas.relacionamentoentreentidade.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.atividadepraticaintegradoratecnologiasdisruptivas.relacionamentoentreentidade.model.Instrutor;
+import com.atividadepraticaintegradoratecnologiasdisruptivas.relacionamentoentreentidade.service.InstrutorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/instrutores")
 public class InstrutorController {
+
+    @Autowired
+    private InstrutorService instrutorService;
+
+    @GetMapping
+    public ResponseEntity<List<Instrutor>> listarTodosInstrutores() {
+        List<Instrutor> instrutores = instrutorService.encontrarTodosInstrutores();
+        if (instrutores.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(instrutores, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Instrutor> buscarInstrutorPorId(@PathVariable Integer id) {
+        Optional<Instrutor> instrutor = instrutorService.encontrarInstrutorPorId(id);
+        return instrutor.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Instrutor> salvarInstrutor(@RequestBody Instrutor instrutor) {
+        Instrutor instrutorSalvo = instrutorService.salvarInstrutor(instrutor);
+        return new ResponseEntity<>(instrutorSalvo, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Instrutor> atualizarInstrutor(@PathVariable Integer id, @RequestBody Instrutor instrutorAtualizado) {
+        Optional<Instrutor> instrutor = instrutorService.encontrarInstrutorPorId(id);
+        if (instrutor.isPresent()) {
+            instrutorAtualizado.setId(id);
+            Instrutor instrutorSalvo = instrutorService.salvarInstrutor(instrutorAtualizado);
+            return new ResponseEntity<>(instrutorSalvo, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deletarInstrutor(@PathVariable Integer id) {
+        Optional<Instrutor> instrutor = instrutorService.encontrarInstrutorPorId(id);
+        if (instrutor.isPresent()) {
+            instrutorService.deletarInstrutor(instrutor.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- src/main/java/com/atividadepraticaintegradoratecnologiasdisruptivas/relacionamentoentreentidade/controller/TurmaController.java
+++ src/main/java/com/atividadepraticaintegradoratecnologiasdisruptivas/relacionamentoentreentidade/controller/TurmaController.java
@@ -1,7 +1,72 @@
 package com.atividadepraticaintegradoratecnologiasdisruptivas.relacionamentoentreentidade.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.atividadepraticaintegradoratecnologiasdisruptivas.relacionamentoentreentidade.model.Turma;
+import com.atividadepraticaintegradoratecnologiasdisruptivas.relacionamentoentreentidade.service.TurmaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/turmas")
 public class TurmaController {
+
+    private final TurmaService turmaService;
+
+    public TurmaController(TurmaService turmaService) {
+        this.turmaService = turmaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Turma>> listarTurmas() {
+        List<Turma> turmas = turmaService.encontrarTodasTurmas();
+        if (turmas.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok(turmas);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Turma> buscarTurmaPorId(@PathVariable Integer id) {
+        Optional<Turma> turma = turmaService.buscarTurmaPorId(id);
+        return turma.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Turma> criarTurma(@RequestBody Turma turma) {
+        Turma turmaSalva = turmaService.salvarTurma(turma);
+        return ResponseEntity.status(HttpStatus.CREATED).body(turmaSalva);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Turma> atualizarTurma(@PathVariable Integer id, @RequestBody Turma turma) {
+        Optional<Turma> turmaExistente = turmaService.buscarTurmaPorId(id);
+        if (turmaExistente.isPresent()) {
+            Turma turmaAtualizada = turmaExistente.get();
+            turmaAtualizada.setDuracao(turma.getDuracao());
+            turmaAtualizada.setHorario(turma.getHorario());
+            turmaAtualizada.setDataInicio(turma.getDataInicio());
+            turmaAtualizada.setDataFim(turma.getDataFim());
+            turmaAtualizada.setInstrutor(turma.getInstrutor());
+            turmaService.salvarTurma(turmaAtualizada);
+            return ResponseEntity.ok(turmaAtualizada);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarTurma(@PathVariable Integer id) {
+        Optional<Turma> turma = turmaService.buscarTurmaPorId(id);
+        if (turma.isPresent()) {
+            turmaService.deletarTurma(turma.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
